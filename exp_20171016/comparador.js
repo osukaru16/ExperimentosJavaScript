c1a979e774cd8e1c2ba9fc92b5197830bc0c26a4
osukaru16 <osukaru16@gmail.com>
2017-10-16 12:55:55 +0200

    Add files via upload

---
+++ exp_20171016/comparador.js
@@ -0,0 +1,291 @@
+
+
+
+
+
+
+function Comparador(){
+	this.nombreCookie = "compararRecursos";
+	
+
+	if (this.leerCookie(this.nombreCookie) === null) {
+		this.listaObjetos = new Array();
+		
+	}else{
+		this.listaObjetos = this.leerCookie(this.nombreCookie);
+
+	}
+
+
+
+
+}
+
+Comparador.prototype.addRecurso = function(objeto){
+
+
+	if (!this.comprobarRecursoExiste(objeto)) {
+		this.listaObjetos.push(objeto);
+		this.escribirCookie(this.nombreCookie,this.listaObjetos);
+		return true;
+	}else{
+		return false;
+	}
+
+}
+
+Comparador.prototype.comprobarRecursoExiste = function(objeto){
+	
+	for (var i =0; i < this.listaObjetos.length ;  i++) {
+		if (this.listaObjetos[i].id === objeto.id) {
+			return true;
+		}
+	}
+
+	return false;
+	
+	
+}
+
+
+
+Comparador.prototype.leerCookie = function(nombre) {
+
+		  var nombreEQ = nombre + "="; 
+		  var coma = document.cookie.split(';');
+
+		  for(var i=0;i < coma.length;i++) {
+
+		    var cookie = coma[i];
+		    while (cookie.charAt(0)==' ') cookie = cookie.substring(1,cookie.length);
+		    if (cookie.indexOf(nombreEQ) == 0) {
+		      return this.descodificarObjecto(decodeURIComponent( cookie.substring(nombreEQ.length,cookie.length) ));
+		    }
+
+		  }
+
+		  return null;
+
+}
+
+Comparador.prototype.escribirCookie = function(nombreCookie, objeto) {
+
+	document.cookie = nombreCookie+"="+this.codificarObjecto(objeto);
+
+}
+
+
+Comparador.prototype.codificarObjecto = function(objeto){
+	return JSON.stringify(objeto);
+} 
+
+
+Comparador.prototype.descodificarObjecto = function(objeto){
+	return JSON.parse(objeto);
+} 
+
+
+
+
+
+
+
+Comparador.prototype.pintarTabla = function(id){
+
+	//var textoHtml = "";
+
+	var textoHtml = "<table><thead> <tr><th></th>";
+	for (var i = 0; i < this.listaObjetos.length; i++) {
+		//alert(this.listaObjetos[i].nom);
+		textoHtml += "<th>"+this.listaObjetos[i].nom+"</th>";
+		//textoHtml += "<th>"+i+"</th>";
+		//alert(this.listaObjetos.length);
+	}
+
+
+	textoHtml += "</tr></thead><tbody>";
+
+
+// medio____________________
+
+
+	//textoHtml += "<tr>";
+	var listaKeys = this.getKeys()
+	//var listaKeysArrays = this.getKeysArray();
+	var listaValores
+	for (var i = 0; i < this.listaObjetos.length ; i++) {
+		
+		for (var j = 2; j < listaKeys.length ; j++) {
+			if (Array.isArray(listaValores[j])){
+
+
+
+
+			}else{
+
+
+			}
+		}
+
+	}
+
+
+
+
+
+
+
+
+
+//Lugar______________________
+
+	textoHtml += "<tr><td>Territori</td>";
+	for (var i = 0; i < this.listaObjetos.length; i++) {
+		textoHtml += "<td>"+this.listaObjetos[i].territori+"</td>";
+	}
+
+	textoHtml += "</tr></tbody></table>";
+
+/*
+	var terminar = false;
+	while(!terminar){
+
+	
+
+	}
+
+*/
+
+	//textoHtml = "hey";
+
+	//alert(textoHtml);
+	document.getElementById(id).innerHTML = textoHtml;
+
+
+}
+
+
+
+
+
+Comparador.prototype.pintaFilasArray = function(posicion){	
+	var texto = "";
+	var listaKeys = this.getKeys();
+	//var listaValores = this.getValor(posicion);
+	//this.listaObjetos
+	//var valorArray = listaValores[posicion];
+
+	texto += "<tr><td>"+listaKeys[posicion]+"</td>";
+	for (var i = 0; i < this.listaObjetos.length; i++) {
+		listaValores = this.getValor(i);
+		
+
+	}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+Comparador.prototype.getKeys = function(){	
+	if (this.listaObjetos.length > 0) {
+		var listaKeys =  Object.keys(this.listaObjetos[0]);
+		return listaKeys;
+	}else{
+		return null;
+	}
+	//return Object.keys(this.listaObjetos[0]);
+}
+
+
+
+
+
+
+
+/*
+Comparador.prototype.getRecurso = function(){
+
+	
+}
+*/
+
+
+
+Comparador.prototype.getValor = function(posicion){
+	if (this.listaObjetos.length > 0) {
+		var listaValores =  Object.values(this.listaObjetos[posicion]);
+		return listaValores;
+	}else{
+		return null;
+	}
+}
+
+
+
+
+
+
+/*getKeysArray busca entre las keys del objeto y te devuelve las que son arrays 
+
+
+*/
+Comparador.prototype.getKeysArray = function(){
+
+	var listaKeys = this.getKeys();
+	var listaValores = this.getValor();
+	var listaKeysArrays = new Array();
+	
+
+	for (var i =0 ; i < listaKeys.length ; i++) {
+		if (Array.isArray(listaValores[i])){
+			listaKeysArrays.push(listaKeys[i]);
+			//alert(listaKeys[i]);
+		}
+		//alert();
+
+
+	}
+
+	return listaKeysArrays;
+
+
+
+}
+
+
+
+
+
+/*
+Comparador.prototype.getNombres = function(){
+
+	for (var i = 0; i < this.listaObjetos.length; i++) {
+			alert(this.listaObjetos[i].nom);
+	}
+
+
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
