dc6f3e995a2b02c0ec4add5a27d369e6e220b82c
osukaru16 <osukaru16@gmail.com>
2017-10-18 12:55:23 +0200

    Add files via upload

---
+++ exp_20171018/comparador.js
@@ -0,0 +1,455 @@
+
+
+
+
+
+
+function Comparador(){
+	this.nombreCookie = "compararRecursos";
+	
+
+	if (this.leerCookie(this.nombreCookie) === null) {
+		this.listaObjetos = new Array();
+		
+	}else{
+		this.listaObjetos = this.leerCookie(this.nombreCookie);
+
+	}
+
+
+
+
+}
+
+Comparador.prototype.addRecurso = function(objeto){
+
+
+	if (!this.comprobarRecursoExiste(objeto)) {
+		this.listaObjetos.push(objeto);
+		this.escribirCookie(this.nombreCookie,this.listaObjetos);
+		return true;
+	}else{
+		return false;
+	}
+
+}
+
+Comparador.prototype.comprobarRecursoExiste = function(objeto){
+	
+	for (var i =0; i < this.listaObjetos.length ;  i++) {
+		if (this.listaObjetos[i].id === objeto.id) {
+			return true;
+		}
+	}
+
+	return false;
+	
+	
+}
+
+
+
+Comparador.prototype.leerCookie = function(nombre) {
+
+		  var nombreEQ = nombre + "="; 
+		  var coma = document.cookie.split(';');
+
+		  for(var i=0;i < coma.length;i++) {
+
+		    var cookie = coma[i];
+		    while (cookie.charAt(0)==' ') cookie = cookie.substring(1,cookie.length);
+		    if (cookie.indexOf(nombreEQ) == 0) {
+		      return this.descodificarObjecto(decodeURIComponent( cookie.substring(nombreEQ.length,cookie.length) ));
+		    }
+
+		  }
+
+		  return null;
+
+}
+
+Comparador.prototype.escribirCookie = function(nombreCookie, objeto) {
+
+	document.cookie = nombreCookie+"="+this.codificarObjecto(objeto);
+
+}
+
+
+Comparador.prototype.codificarObjecto = function(objeto){
+	return JSON.stringify(objeto);
+} 
+
+
+Comparador.prototype.descodificarObjecto = function(objeto){
+	return JSON.parse(objeto);
+} 
+
+
+
+
+
+/*Nota la id que pide el metodo pintarTabla es la id
+de la etiqueta html*/
+
+Comparador.prototype.pintarTabla = function(id){
+	document.getElementById(id).innerHTML = this.procesoPintarTabla();
+}
+
+
+
+
+
+
+Comparador.prototype.procesoPintarTabla = function(){
+
+
+	if (this.listaObjetos.length === 0) {
+		var textoHtml = "<p> No hay objetos o se han borrado las cookies </p>";
+		return textoHtml;
+	}
+
+	
+	// cabecera________
+	var textoHtml = "<table><thead> <tr><th></th>";
+	for (var i = 0; i < this.listaObjetos.length; i++) {
+		textoHtml += "<th>"+this.listaObjetos[i].nom+"</th>";
+	}
+
+
+	textoHtml += "</tr></thead><tbody>";
+
+
+	// medio____________________
+
+
+	// todas las arrays
+	for (var i = 0; i < this.getKeysArray(0).length; i++) {
+		textoHtml += this.pintaFilasTituloArray(i);
+		textoHtml += this.pintaFilasArray(i);
+	}
+
+	
+	//Precio
+
+	textoHtml += "<tr><td>Gratuit</td>";
+	for (var i = 0; i < this.listaObjetos.length; i++) {
+			textoHtml += "<td>"+this.pintarValorOX(this.listaObjetos[i].gratuit)+"</td>";
+	}
+	textoHtml += "</tr>";
+
+
+
+	//Lugar______________________
+
+	textoHtml += "<tr><td>Territori</td>";
+	for (var i = 0; i < this.listaObjetos.length; i++) {
+		textoHtml += "<td>"+this.listaObjetos[i].territori+"</td>";
+	}
+	textoHtml += "</tr></tbody></table>";
+
+	return textoHtml;
+
+
+}
+
+
+
+/*
+	pintaFilasTituloArray:
+	Se ha creado para reducir el codigo de pintaFilasArray y posibles bugs.
+*/
+
+
+Comparador.prototype.pintaFilasTituloArray = function(posicion){	
+	var textoHtml = "";
+	var listaKeysArrays = this.getKeysArray(0);
+
+
+	textoHtml += "<tr><td>"+listaKeysArrays[posicion]+"</td>";
+	for (var i = 0; i < this.listaObjetos.length; i++) {
+		textoHtml += "<td>   </td>";
+	}
+	textoHtml += "</tr>";
+
+	return textoHtml;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+	pintaFilasArray:
+	Devuelve un String con el html necesario para 
+	pintar el array dereminado por la posicion de la array de keys que devuelve
+	el metodo this.getKeysArray().
+	nota: usa un for con this.getKeysArray().length
+
+*/
+
+Comparador.prototype.pintaFilasArray = function(posicion){	
+	var textoHtml = "";
+	var keyArray = this.getKeysArray(0)[posicion];
+
+	for (var j = 0; j < this.valoresArray(keyArray).length; j++) {
+		textoHtml += "<tr>";
+		//textoHtml += "<td>"+this.listaObjetos[listaKeysArrays[posicion]][contador]+"</td>";
+		//textoHtml += "<td>"+this.listaObjetos[keyArray][contador]+"</td>";
+		textoHtml += "<td>"+this.valoresArray(keyArray)[j]+"</td>";
+		var comprobadorValores = this.comprobadorValor(keyArray, this.valoresArray(keyArray)[j]);
+		for (var g = 0; g < comprobadorValores.length; g++) {
+			textoHtml += "<td>"+this.pintarValorOX(comprobadorValores[g])+"</td>";
+		}
+			
+		textoHtml += "</tr>";
+	}
+
+	return textoHtml;
+}
+
+
+
+
+
+/*
+
+Nota:
+Aqui hay que escribir un metodo que mire en en la array ejemplo tipus y que 
+salga una array con los valores sin repetir ninguno
+
+valoresArray: lugar(Nombre del array)
+
+
+*/
+Comparador.prototype.valoresArray = function(lugar){
+	var listaValores = new Array();
+	for (var i = 0; i < this.listaObjetos.length; i++) {
+		//alert(this.listaObjetos[i][lugar].length);
+		//alert(this.listaObjetos[0][lugar][1]);
+		for (var j = 0 ; j < this.listaObjetos[i][lugar].length ;  j++) {
+			if ((listaValores.indexOf(this.listaObjetos[i][lugar][j])) === -1 ){
+				listaValores.push(this.listaObjetos[i][lugar][j]);
+			}
+		}		
+	}
+
+	return listaValores;
+}
+
+
+
+
+
+
+
+/*
+	arrayPintar:
+	Es para facilitar el proceso de pintado de la pagina
+	devuelve un array con todos los elementos necesarios
+*/
+/*
+Comparador.prototype.arrayPintar = function(){
+	var pintaLista = new Array();
+
+	for (var i = 0; i < this.listaObjetos.length; i++) {
+		var contador = 0;
+		for (var valor in this.listaObjetos[i]) {
+			pintaLista[i][contador] = valor;
+			contador++;
+		}
+	}
+	return pintaLista;
+	
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+	comprobadorValor:
+	comprueba si el valor(String) que se quiere esta en la array(lugar(Nombre del array)) que se quiere
+	devuelve un array booleana
+
+*/
+
+Comparador.prototype.comprobadorValor = function(lugar, valor){
+	var listaValores = new Array();
+	for (var i = 0; i < this.listaObjetos.length; i++) { 
+		if ((this.listaObjetos[i][lugar].indexOf(valor)) !== -1 ){
+			listaValores.push(true);
+		}else{
+			listaValores.push(false);
+		}
+
+	}
+	return listaValores;
+}
+
+
+
+Comparador.prototype.pintarValorOX = function(valor){
+	if (valor) {
+		return "O";
+	}
+	return "X";
+}
+
+
+
+
+
+
+
+
+
+Comparador.prototype.getKeys = function(posicion){	
+	if (this.listaObjetos.length > 0) {
+		var listaKeys =  Object.keys(this.listaObjetos[posicion]);
+		return listaKeys;
+	}else{
+		return null;
+	}
+	//return Object.keys(this.listaObjetos[0]);
+}
+
+
+
+
+
+
+
+/*
+Comparador.prototype.getRecurso = function(){
+
+	
+}
+*/
+
+
+
+
+
+
+
+/* 
+	getValor:
+	Devuelve un array con e los valores de la posicion del Array de objetos. 
+
+*/
+
+Comparador.prototype.getValor = function(posicion){
+	var listaValores = new Array();
+	if (this.listaObjetos.length > 0) {
+		listaValores =  Object.values(this.listaObjetos[posicion]);
+		return listaValores;
+	}
+		
+	//alert(listaValores.length);
+	return listaValores;
+	
+}
+
+
+
+/*
+Comparador.prototype.getValor = function(){
+	if (this.listaObjetos.length > 0) {
+		var listaValores =  Object.values(this.listaObjetos[0]);
+		return listaValores;
+	}else{
+		return null;
+	}
+}
+*/
+
+
+
+
+
+
+
+
+
+/*
+	getKeysArray: 
+	Busca entre las keys del objeto y te devuelve las que son arrays 
+	pero tienes que decir en que posicion de la array de objetos 
+
+*/
+
+Comparador.prototype.getKeysArray = function(posicion){
+
+	var listaKeys = this.getKeys(posicion);
+	var listaValores = this.getValor(posicion);
+	var listaKeysArrays = new Array();
+	
+	for (var i =0 ; i < listaKeys.length ; i++) {
+		if (Array.isArray(listaValores[i])){
+			listaKeysArrays.push(listaKeys[i]);
+		}
+		
+
+
+	}
+
+	return listaKeysArrays;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Comparador.prototype.getNombres = function(){
+
+	for (var i = 0; i < this.listaObjetos.length; i++) {
+			alert(this.listaObjetos[i].nom);
+	}
+
+
+}
+*/
+
+
+
+/*
+
+
+Comparador.prototype.getArray = function(){
+	return this.listaObjetos;
+}
+
+*/
+
+
+
+
+
+
